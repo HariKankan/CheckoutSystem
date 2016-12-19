01db6e3a8fbfb646804fe216d274e0db736a2e35
Hari Kankan <kankanampati1@cmegroup.com>
2016-12-19 23:25:50 +0000

    CheckoutSystemtests implementation

---
+++ CheckoutSystem/Test/CheckOutSystemTest.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutSystem
+{
+    [TestFixture]
+    public class TestClass2
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+            Assert.AreEqual(1, 1, "adsajksakjd");
+        }
+
+        [Test]
+        public void shouldPrintReceiptForShoppingCartContaingApples()
+        {
+
+            // ShoppingCart.
+            ShoppingCart a = new ShoppingCart();
+
+
+            Assert.AreEqual(a.printReceipt("Apple"), "0.60", "1 Apple");
+            Assert.AreEqual(a.printReciept(("Apple", "Apple")), "1.20", "2 Apple");
+            Assert.AreEqual(a.printReciept(("Apple", "Apple", "pple")), "1.80", "3 Apple");
+
+        }
+
+
+        [Test]
+        public void shouldPrintReceiptForShoppingCartContaingOranges()
+        {
+
+            // ShoppingCart.
+            ShoppingCart a = new ShoppingCart();
+
+
+            Assert.AreEqual(a.printReceipt("Orange"), "0.25", "1 Orange");
+            Assert.AreEqual(a.printReciept(("Orange", "Orange")), "0.50", "2 Orange");
+            Assert.AreEqual(a.printReciept(("Orange", "Orange", "Orange")), "0.75", "3 Orange");
+
+        }
+
+
+        [Test]
+        public void shouldPrintReceiptForShoppingCartContaingAppleandOranges()
+        {
+
+            // ShoppingCart.
+            ShoppingCart a = new ShoppingCart();
+
+
+            Assert.AreEqual(a.printReceipt("Apple"), "0.60", "1 Apple");
+            Assert.AreEqual(a.printReciept(("Apple", "Orange")), "0.85", "1 Apple, 1 Orange");
+            Assert.AreEqual(a.printReciept(("Apple", "Orange", "A   pple")), "1.45", "2 Apple , 1 Orange");
+
+        }
+
+
+    }
+}
---
+++ CheckoutSystem/src/ShoppingCart.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace CheckoutSystem
+{
+     class ShoppingCart
+    {
+
+
+        private static double APPLE_PRICE= 0.60;
+        private static double ORANGE_PRICE = 0.25;
+
+        private static String APPLE = "Apple";
+        private static String ORANGE = "Orange";
+
+        public int ScanItems(List<String> shoppingcart)
+        
+        {
+
+            throw new NotImplementedException();
+             // code to developed
+                
+          }
+
+        //   public String PrintReceipt(List<String> shoppingCart)
+
+        //   public static void PrintReceipt(List<string> shoppingCart)
+        //public void PrintReceipt(String item1)
+        //{
+        //    // Code to be developed
+        //    throw new NotImplementedException();
+        //    //  return shoppingCart;     
+
+
+        //}
+        //public void PrintReceipt(String item1, String item2)
+        //{
+        //    // Code to be developed
+        //    throw new NotImplementedException();
+        //    //  return shoppingCart;     
+
+
+        //}
+        //public void PrintReceipt(String item1, String item2, string item3)
+        //{
+        //    // Code to be developed
+        //    throw new NotImplementedException();
+        //    //  return shoppingCart;     
+
+
+        //}
+
+            // We can also implement the below using List arrays
+
+        internal double printReceipt(string v)
+        {
+            throw new NotImplementedException();  // Dev code not implemented
+        }
+
+        internal double printReciept(string v1, string v2)
+        {
+            throw new NotImplementedException();   // Dev code not implemented
+        }
+
+        internal double printReciept(string v1, string v2, string v3)
+        {
+            throw new NotImplementedException();  // Dev code not implemented
+        }
+
+
+    }
+}
